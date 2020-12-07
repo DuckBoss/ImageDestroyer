c77c0e69f02d12fbb92ab172d1b5bb0cdaf8110b
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-12-06 19:24:16 -0500

    fixed some issues

--- rip_image.py
+++ rip_image.py
@@ -3,26 +3,26 @@ from shutil import copyfile
 import os
 
 
-fileName = "imageName"
+fileName = "image_name"
 
-count = 0
-maxIters = 25
+count = 1
+maxIters = 15
 copysrc = "%s.jpg" % fileName
 copydst = "%s_copy.jpg" % fileName
 
 copyfile(copysrc, copydst)
 
 for x in range(maxIters+1, 1, -1):
-    MainImage = Image.open("%s.jpg" % fileName)
+    MainImage = Image.open("%s_copy.jpg" % fileName)
     width, height = MainImage.size
-    buffer = os.path.join("%s.jpg" % fileName)
-    MainImage = MainImage.resize((150, 150), Image.NEAREST)
+    buffer = os.path.join("%s_copy.jpg" % fileName)
+    MainImage = MainImage.resize((int(width/4), int(height/4)), Image.NEAREST)
     if x % 2 == 0:
         newQual = x*2
     else:
         newQual = x
     MainImage.save(buffer, "JPEG", quality=newQual)
-    MainImage = Image.open("%s.jpg" % fileName)
+    MainImage = Image.open("%s_copy.jpg" % fileName)
     MainImage = MainImage.resize((width, height), Image.NEAREST)
     MainImage.save(buffer)
     count += 1
